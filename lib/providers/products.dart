ff951348ed4280d1644c316510ddda2acada5451
matheus costa <matheuscosta.tech@gmail.com>
2022-07-10 10:35:58 -0300

    Creating logic to show favorites only or all

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -5,8 +5,14 @@ import '../dummy_data.dart';
 
 class Products with ChangeNotifier {
   final List<Product> _items = dummyData;
+  bool _showFavoritesOnly = false;
 
-  List<Product> get items => [..._items];
+  List<Product> get items {
+    if (_showFavoritesOnly) {
+      return _items.where((product) => product.isFavorite).toList();
+    }
+    return [..._items];
+  }
 
   void addProduct() {
     // _items.add();
@@ -16,5 +22,15 @@ class Products with ChangeNotifier {
 
   Product findById(String id) {
     return _items.firstWhere((element) => element.id == id);
+  }
+
+  void setShowFavoritesOnly() {
+    _showFavoritesOnly = true;
+    notifyListeners();
+  }
+
+  void setShowAll() {
+    _showFavoritesOnly = false;
+    notifyListeners();
   }
 }
--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -1,14 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/products.dart';
 import '../widgets/products_grid.dart';
+
+enum OptionsFilter {
+  Favorites,
+  All,
+}
 
 class ProductsOverviewScreen extends StatelessWidget {
   const ProductsOverviewScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final products = Provider.of<Products>(context, listen: false);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Shop'),
+        actions: [
+          PopupMenuButton(
+            onSelected: (OptionsFilter value) {
+              if (value == OptionsFilter.Favorites) {
+                products.setShowFavoritesOnly();
+              } else {
+                products.setShowAll();
+              }
+            },
+            icon: const Icon(Icons.more_vert),
+            itemBuilder: (_) => [
+              PopupMenuItem(
+                  child: Text('Only favorites'),
+                  value: OptionsFilter.Favorites),
+              PopupMenuItem(child: Text('Show all'), value: OptionsFilter.All),
+            ],
+          ),
+        ],
       ),
       body: const ProductsGrid(),
     );
