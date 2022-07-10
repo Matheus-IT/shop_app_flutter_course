f81c9221c3f3f217716406fa448e418f010f8299
matheus costa <matheuscosta.tech@gmail.com>
2022-07-09 22:32:02 -0300

    Using consumer to improve performance

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -17,19 +17,21 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final product = Provider.of<Product>(context);
+    final product = Provider.of<Product>(context, listen: false);
 
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
         footer: GridTileBar(
           backgroundColor: Colors.black87,
-          leading: IconButton(
-            icon: (product.isFavorite)
-                ? const Icon(Icons.favorite)
-                : const Icon(Icons.favorite_border),
-            color: Theme.of(context).accentColor,
-            onPressed: () => product.toggleFavoriteStatus(),
+          leading: Consumer<Product>(
+            builder: (ctx, product, _) => IconButton(
+              icon: (product.isFavorite)
+                  ? const Icon(Icons.favorite)
+                  : const Icon(Icons.favorite_border),
+              color: Theme.of(context).accentColor,
+              onPressed: () => product.toggleFavoriteStatus(),
+            ),
           ),
           trailing: IconButton(
             icon: const Icon(Icons.shopping_cart),
