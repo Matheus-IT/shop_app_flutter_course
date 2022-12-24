97381e5e12b7bf24e237515cd717a5812a2ebef1
matheus costa <matheuscosta.tech@gmail.com>
2022-12-24 12:57:26 -0300

    New handle method for controller

--- lib/controllers/orders_controller.dart
+++ lib/controllers/orders_controller.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/widgets.dart';
 import 'package:provider/provider.dart';
+import 'package:shop_app_flutter_course/external/firebase_operations/request_add_order.dart';
 import 'package:shop_app_flutter_course/providers/cart.dart';
 import 'package:shop_app_flutter_course/providers/orders.dart';
 
@@ -9,13 +10,13 @@ class OrdersController {
     required List<CartItem> cartItemsList,
     required double totalAmount,
     required Function() clearCart,
-  }) {
+  }) async {
     final ordersProvider = Provider.of<Orders>(context, listen: false);
-    // add persistence here
-    ordersProvider.addOrder(
-      cartItemsList,
-      totalAmount,
-    );
+
+    final whenAdded = await requestAddOrderFirebase(cartItemsList, totalAmount);
+
+    ordersProvider.addOrder(cartItemsList, totalAmount, whenAdded);
+
     clearCart();
   }
 }
