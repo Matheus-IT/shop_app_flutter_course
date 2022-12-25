b2758410d54966a80ad03314adc42d07e8dffaba
matheus costa <matheuscosta.tech@gmail.com>
2022-12-25 14:16:00 -0300

    Refactoring controllers to adopt the entities pattern

--- lib/controllers/orders_controller.dart
+++ lib/controllers/orders_controller.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/widgets.dart';
 import 'package:provider/provider.dart';
-import 'package:shop_app_flutter_course/external/firebase_operations/request_add_order.dart';
-import 'package:shop_app_flutter_course/providers/cart.dart';
-import 'package:shop_app_flutter_course/providers/orders.dart';
-import 'package:shop_app_flutter_course/usecases/fetch_all_orders_from_remote_datasource.dart';
+
+import '../entities/cart.dart';
+import '../external/firebase_operations/request_add_order.dart';
+import '../providers/orders.dart';
+import '../usecases/fetch_all_orders_from_remote_datasource.dart';
 
 class OrdersController {
   static Future<void> handleNewOrder(
     BuildContext context, {
-    required List<CartItem> cartItemsList,
+    required List<Cart> cartItemsList,
     required double totalAmount,
     required Function() clearCart,
   }) async {
