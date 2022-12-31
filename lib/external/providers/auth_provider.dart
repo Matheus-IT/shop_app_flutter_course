72cfe139313c9fe2e4316e0417e1953fce69cad3
matheus costa <matheuscosta.tech@gmail.com>
2022-12-31 00:06:50 -0300

    Basic auth provider

--- lib/external/providers/auth_provider.dart
+++ lib/external/providers/auth_provider.dart
@@ -1,26 +1,8 @@
-import 'dart:convert';
-
 import 'package:flutter/widgets.dart';
-import 'package:http/http.dart' as http;
-import '../firebase_operations/get_signup_url.dart';
+import '../firebase_operations/request_signup.dart';
 
 class AuthProvider extends ChangeNotifier {
   late String _token;
   late DateTime _expireDate;
   late String _userId;
-
-  Future<void> signup(String email, String password) async {
-    // final url = Uri.parse('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=<YOUR_API_KEY>');
-    final url = getSignUpUrl();
-    final response = await http.post(
-      url,
-      body: json.encode({
-        'email': email,
-        'password': password,
-        'returnSecureToken': true,
-      }),
-    );
-
-    print(json.decode(response.body));
-  }
 }
